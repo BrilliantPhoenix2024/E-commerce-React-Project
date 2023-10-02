764f892a48252923fbd3a21c176725e0da2507d7
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-10-02 11:28:57 +0330

    E-commerce Homepage and add some styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
+
+import HomePage from "./components/homePage/homepage.components";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
