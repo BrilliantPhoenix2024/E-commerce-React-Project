e1ea431e296bfa4d55d5e6029c6c83eef180f12c
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-10-05 20:50:45 +0330

    Routing the Project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 import HomePage from "./pages/homePage/homepage.components";
 import "./App.css";
 
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  );
+};
+
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Routes>
+        <Route exact path="/" Component={HomePage} />
+        <Route path="/hats" Component={HatsPage} />
+      </Routes>
     </div>
   );
 }
